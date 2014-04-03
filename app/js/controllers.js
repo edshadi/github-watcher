8adfc15da7d549d10dbe0c933f93800ce06e8e97
Ed Shadi <edshadi@gmail.com>
2014-04-02 20:46:19 -0700

    create new watchlist

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,20 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('WatchlistsController', ['$scope', function($scope) {
-    $scope.watchlists = [
-      {name: "my list"}
-    ]
-  }])
-  .controller('CreateWatchlistController', ['$scope', function($scope) {
-
-  }])
+.controller('WatchlistsController', ['$scope', function($scope) {
+  $scope.watchlists = [
+    {name: "my list"}
+  ]
+}])
+.controller('CreateWatchlistController', ['$scope', '$http', function($scope, $http) {
+  $scope.save = function(watchlist) {
+    $http.post('/watchlists', watchlist)
+    .success(function(data, status, headers, config) {
+      console.log(data);
+    })
+    .error(function(data, status, headers, config) {
+      console.log(data);
+    });
+  }
+}])
 ;
