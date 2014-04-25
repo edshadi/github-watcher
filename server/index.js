4deaa7889506d8ed284bf7481c7e1c63a52c39ab
Ed Shadi <edshadi@gmail.com>
2014-04-25 11:38:55 -0700

    logger dev

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ var application_root = __dirname,
 var app = express();
 
 app.configure( function() {
-  app.use( express.logger() );
+  app.use( express.logger('dev') );
   app.use( express.bodyParser() );
   app.use( express.methodOverride() );
   app.use( app.router );
