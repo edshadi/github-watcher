5b76ce6f4a7919c3b00844fd122a7333e79ce999
Ed Shadi <edshadi@gmail.com>
2014-04-25 11:39:14 -0700

    use config for mongo url

--- server/models/index.js
+++ server/models/index.js
@@ -1,2 +1,9 @@
 var requireDirectory = require('require-directory');
 module.exports = requireDirectory(module);
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    config = require('../configuration')
+;
+
+mongoose.connection.open(config.get('mongo:url'))
